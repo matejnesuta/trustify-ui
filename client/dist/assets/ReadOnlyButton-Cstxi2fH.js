0ab5e276b567e8cc97a395dc6b8f5af91a0ee43d
matejnesuta <matejnesuta@users.noreply.>
2026-09-09 11:24:08 +0000

    Checkin dist dir

---
+++ client/dist/assets/ReadOnlyButton-Cstxi2fH.js
@@ -0,0 +1,1 @@
+import{r as e}from"./rolldown-runtime-QTnfLwEv.js";import{i as t,t as n}from"./react-C_KK3IH-.js";import{on as r}from"./useBranding-C0E3wMAZ.js";import{l as i}from"./index-FMIN6lxV.js";var a=e(t(),1),o=n(),s=e=>{let{areMutationsDisabled:t}=a.useContext(i);return(0,o.jsx)(r,{...e,isDisabled:t})};export{s as t};
